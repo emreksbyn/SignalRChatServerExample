af004195a33c60507949d431ddc6dd5e3a5a05f9
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-08-15 01:15:34 +0300

    NickName and Connection Id mapped.

--- SignalRChatServerExample/Hubs/ChatHub.cs
+++ SignalRChatServerExample/Hubs/ChatHub.cs
@@ -1,8 +1,18 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SignalRChatServerExample.Models;
+using System.Threading.Tasks;
 
 namespace SignalRChatServerExample.Hubs
 {
     public class ChatHub : Hub
     {
+        public async Task GetNickName(string nickName)
+        {
+            Client client = new Client
+            {
+                ConnectionId = Context.ConnectionId,
+                NickName = nickName
+            };
+        }
     }
 }
